f7d09fa803c70daebe79d7b843098ded56531548
Piotr Polanski <polanski.piotr@me.com>
2018-06-01 17:05:56 +0000

    #11 phoenix <-> react integration

--- src/web/router.ex
+++ src/web/router.ex
@@ -16,7 +16,7 @@ defmodule Src.Router do
   scope "/", Src do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
